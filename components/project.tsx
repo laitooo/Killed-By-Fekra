4c8236f1122df7f541570faa730066a9cc754c02
Laitooo <killuasan25@gmail.com>
2022-12-25 01:21:12 +0200

    update hpme ppage ui

--- components/project.tsx
+++ components/project.tsx
@@ -7,9 +7,9 @@ const ProjectCard = (probs: {project: Project}) => {
         <figure>
           <img src={probs.project.logo} alt={probs.project.name} className="mt-6 w-40 h-40" />
         </figure>
-        <div className="card-body h-60 xl:h-80">
-          <h2 className="mt-8 text-white text-2xl font-semibold">{probs.project.name}</h2>
-          <p className="mt-4 text-base text-justify">{probs.project.description}</p>
+        <div className="card-body xl:h-80">
+          <h2 className="mt-4 text-white text-2xl font-semibold">{probs.project.name}</h2>
+          <p className="mt-2 text-base text-justify">{probs.project.description}</p>
           <p className="mt-4 text-base text-justify">Killed at {probs.project.date.toDateString()}</p>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,34 +11,35 @@ export default function Home() {
   const router = useRouter()
 
   const openProjectDetails = (id: number) => {
-    // router.push({
-    //   pathname: '/projects/project',
-    //   query: { "id": id },
-    // });
+    router.push({
+      pathname: '/details',
+      query: { "id": id },
+    });
   }
 
   return (
-    <div data-theme="synthwave" className="bg-cover bg-[url('/images/background.jpg')] bg-fixed bg-no-repeat">
-       <Head>
-           <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-           <meta name="keywords" content="portfolio, laitooo, Alzobair, Alzobair Mohammed, nextjs"></meta>
-           <meta name="description" content="Lists of projects killed by Fekra"></meta>
-           <meta property="og:title" content="Killed by Fekra" />
-           <meta property="og:image" content="http://localhost:3000/images/logo.jpg" />
-           <meta charSet="utf-8"></meta>
-           <link rel="icon" href="http://localhost:3000/images/logo.jpg"></link>
-           <title>Killed by Fekra</title>
-       </Head>
-      <div className="backdrop-blur">
-        <div className="grid place-items-center px-20 py-10 text-white text-5xl font-semibold">
+    <html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="keywords" content="Mech, Mobile, Flutter, Projects, Killed, nextjs"></meta>
+        <meta name="description" content="Lists of projects killed by Fekra"></meta>
+        <meta property="og:title" content="Killed by Fekra" />
+        <meta property="og:image" content="http://localhost:3000/images/fekra.jpg" />
+        <meta charSet="utf-8"></meta>
+        <link rel="icon" href="http://localhost:3000/images/fekra.jpg"></link>
+        <title>Killed by Fekra</title>
+      </Head>
+
+      <div data-theme="synthwave" className="min-h-screen">
+        <div className="grid place-items-center py-10 text-white font-semibold">
           <div className="flex flex-row">
-            <p className="mt-12">Killed by </p>
-            <img src="/images/fekra.png" alt="Fekra" className="w-60" />
+            <p className="mt-10 md:mt-12 text-2xl md:text-5xl">Killed by </p>
+            <img src="/images/fekra.png" alt="Fekra" className="w-40 md:w-60" />
           </div>
         </div>
         <div className="grid place-items-center items-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8 px-4 pt-5 pb-20">
           {
-            projectsList.sort((a, b) => (a.id < b.id ? -1 : 1)).map((item, index) => {
+            projectsList.sort((a, b) => (a.date < b.date ? -1 : 1)).map((item, index) => {
               return (
                 <div key={index} onClick={(e) => openProjectDetails(item.id)}>
                   <ProjectCard project={item} />
@@ -48,6 +49,6 @@ export default function Home() {
           }
         </div>
       </div>
-    </div>
+    </html>
   )
 }
