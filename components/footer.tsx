0b3c8834d88fe298a7c3540d42ed58f9b35a81f6
Laitooo <killuasan25@gmail.com>
2022-12-25 04:17:27 +0200

    fix lint issues

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 const Footer = () => {
     return (
       <div className="grid place-items-center py-10 bg-base-300">
           <p className="text-center text-base lg:text-xl px-4 lg:px-20">
-            Killed by Fekra is the Graveyard of a list of a great projects that started in Fekra and wasn't continued or published.<br></br><br></br>
+            Killed by Fekra is the Graveyard of a list of a great projects that started in Fekra and was not continued or published.<br></br><br></br>
           We aim to record history of unfinished projects and keep them as a reminder to keep in mind while doing future projects.<br></br><br></br>
-          You can join the discussion on <a href="https://github.com/laitooo/Killed-By-Fekra" className="text-blue-500">Github</a>.<br></br><br></br>
-          This project was developed by by <a href="https://laitooo.vercel.app"  className="text-blue-500">Alzobair Mohammed (laitooo)</a>.</p>
+          You can join the discussion on <Link href="https://github.com/laitooo/Killed-By-Fekra" className="text-blue-500">Github</Link>.<br></br><br></br>
+          This project was developed by by <Link href="https://laitooo.vercel.app"  className="text-blue-500">Alzobair Mohammed (laitooo)</Link>.</p>
       </div>
     )
   }
